b568ae2098daa1b1b6694accecb1bd1cdf399549
Samiul <sumonmti498@gmail.com>
2021-08-01 02:30:32 +0600

    First commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,60 +1,13 @@
 import 'package:flutter/material.dart';
-import 'package:curved_navigation_bar/curved_navigation_bar.dart';
+import 'flutter_application_1/home_page.dart';
 
 void main() {
-  runApp(MaterialApp(
-    debugShowCheckedModeBanner: false,
-    home: MyBottomBar(),
-  ));
+  runApp(MyApp());
 }
 
-class MyBottomBar extends StatefulWidget {
-  @override
-  State<StatefulWidget> createState() {
-    return MyBottomBarState();
-  }
-}
-
-class MyBottomBarState extends State<MyBottomBar> {
-  Color bgcolor = Colors.blueAccent;
-  var txt = "Login Page";
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: bgcolor,
-      appBar: AppBar(
-        title: Text("Bottom Navber"),
-      ),
-      body: Center(
-        child: Text(
-          txt,
-          style: TextStyle(fontSize: 50.0),
-        ),
-      ),
-      bottomNavigationBar: CurvedNavigationBar(
-        backgroundColor: bgcolor,
-        items: [
-          Icon(Icons.login),
-          Icon(Icons.search),
-          Icon(Icons.app_registration),
-        ],
-        onTap: (index) {
-          setState(() {
-            if (index == 0) {
-              bgcolor = Colors.pinkAccent;
-              txt = "Login Page";
-            }
-            if (index == 1) {
-              bgcolor = Colors.greenAccent;
-              txt = "Search Page";
-            }
-            if (index == 2) {
-              bgcolor = Colors.yellowAccent;
-              txt = "Registration Page";
-            }
-          });
-        },
-      ),
-    );
+    return MaterialApp(debugShowCheckedModeBanner: false, home: HomePage());
   }
 }
